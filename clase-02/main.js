6e33c0a0f563ab14eaff507f8eb82d00da72677e
victor villafane <victor_villafane@microgestion.com>
2026-08-13 10:45:59 -0300

    clase-02

--- clase-02/main.js
+++ clase-02/main.js
@@ -1,31 +1,39 @@
 const http = require("http")
 
 const alumnos = [
-    "Juan Perez",
-    "Pedro Perez",
-    "Homero Simpson"
+    {
+        id: 1,
+        nombre: "Juan Perez",
+        nota: 9
+    },
+    {
+        id: 2,
+        nombre: "Pedro Perez",
+        nota: 8
+    },
+    {
+        id: 3,
+        nombre: "Homero Simpson",
+        nota: 7
+    }
 ]
 
-const server = http.createServer( (request, response) => {
-    console.log(request.url)
-    // if( request.url == "/materia" ){
-    //     response.write("La materia es Aplicaciones Hibridas")
-    // }else if( request.url == "/hola" ){
-    //     response.write("Hola!")
-    // } else{
-    //     response.write("No se que responder")
-    // }
+const server = http.createServer((request, response) => {
     response.write("<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Document</title></head><body>")
-    switch( request.url ){
+    response.write("<h1>Mi espectacular pagina web</h1>")
+    switch (request.url) {
+        case "/":
+            response.write("Mi nombre y apellido")
+            break
         case "/materia":
             response.write("La materia es Aplicaciones Hibridas")
             break
-        case "/hola":
+        case "/profesor":
             response.write("Hola! Victor Villafañe")
             break
         case "/alumnos":
             response.write("<ul>")
-            alumnos.forEach( alumno => response.write("<li>" + alumno +"</li>") )
+            alumnos.forEach(alumno => response.write("<li>Nombre: " + alumno.nombre + " Nota: " + alumno.nota + "</li>"))
             response.write("</ul>")
             break
         default:
@@ -33,6 +41,6 @@ const server = http.createServer( (request, response) => {
             break
     }
     response.end("</body></html>")
-} )
+})
 
 server.listen("2026")
